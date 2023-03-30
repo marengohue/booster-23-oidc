e38739c88c66df7ddf1a9bba8350edefeb75443b
Oyvinkg <oyvind.grutle@kantega.no>
2023-03-30 10:25:45 +0200

    Add credentials

--- oidc-client/core/variables.py
+++ oidc-client/core/variables.py
@@ -7,5 +7,5 @@ RESPONSE_TYPE = 'code'
 GRANT_TYPE = 'authorization_code'
 REDIRECT_URL = 'http://localhost:8000/callback'
 
-CLIENT_ID = ''
-CLIENT_SECRET = ''
+CLIENT_ID = '08cc89e2-59ed-4251-92db-cd554e0acadb'
+CLIENT_SECRET = '5yVTl-rHUEwlErd8xqDWnQ=='
